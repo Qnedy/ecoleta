0556e86cd998cdadbbebc7c133981a4a5e67c35c
qnedy <altmaisefequatro@gmail.com>
2020-06-03 17:12:48 -0300

    server finish

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -47,7 +47,7 @@ class PointsController{
      const trx = await knex.transaction();
 
      const point = {
-      image: 'image-fake',
+      image: 'https://images.unsplash.com/photo-1552353289-97f99c442b3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
       name,
       email,
       whatsapp,
